00788a0df25a3cb62047b81e1e14c22116d64841
Jarno-2003 <jarno.brocker@gmail.com>
2023-12-17 22:00:42 +0100

    scene load to level 1

---
+++ Villa electrica/Assets/Scripts/SceneManagment.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagment : MonoBehaviour
+{
+    public string level1Name;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartLevel1()
+    {
+        SceneManager.LoadScene(level1Name);
+        Debug.Log(SceneManager.GetActiveScene().name);
+    }
+}
