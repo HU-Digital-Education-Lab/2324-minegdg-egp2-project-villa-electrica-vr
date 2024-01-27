11f89d6d4a8c3696c2d1bb90c11144518cd0baba
Appenzeill <daanroth@proton.me>
2024-01-27 01:13:58 +0100

    Cable system finally works, need to turn it into a level.

---
+++ Villa electrica/Assets/Scripts/CircuitController.cs
@@ -0,0 +1,123 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircuitController : MonoBehaviour
+{
+private AudioSource source;
+    public AudioClip errorSound;
+    public AudioClip correctSound;
+    // public int cableID;
+
+    [SerializeField] private Transform[] _NTransforms;
+    [SerializeField] private Transform[] _LTransforms;
+    [SerializeField] private Transform[] _SLTransforms;
+
+    public int _NIndex = 0;
+    public int _LIndex = 0;
+    public int _SLIndex = 0;
+
+    public int _NLength = 0;
+    public int _LLength = 0;
+    public int _SLLength = 0;
+
+    public GameObject Cable;
+    public int _cableIndex = 0;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        // _lineRenderer = GetComponent<LineRenderer>();
+        source = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (_cableIndex == 0)
+        {
+            if (_NTransforms[0] == other.transform && _NTransforms.Length != _NIndex)
+            {
+                _cableIndex = 1;
+                _NIndex = 1;
+                _NLength = _NTransforms.Length;
+                source.PlayOneShot(correctSound);
+            }
+            else if (_LTransforms[0] == other.transform  && _LTransforms.Length != _LIndex)
+            {
+                _cableIndex = 2;
+                _LIndex = 1;
+                _LLength = _LTransforms.Length;
+                source.PlayOneShot(correctSound);
+            }
+            else if (_SLTransforms[0] == other.transform && _SLTransforms.Length != _SLIndex)
+            {
+                _cableIndex = 3;
+                _SLIndex = 1;
+                _SLLength = _SLTransforms.Length;
+                source.PlayOneShot(correctSound);
+            }
+            else 
+            {
+                source.PlayOneShot(errorSound);
+            }
+        }
+        else if (_cableIndex == 1)
+        {
+            if (_NTransforms[_NIndex] == other.transform)
+            {
+                _NIndex += 1;
+                source.PlayOneShot(correctSound);
+
+                if (_NIndex == _NTransforms.Length)
+                {
+                    _cableIndex = 0;
+                }
+            }
+            else 
+            {
+                source.PlayOneShot(errorSound);
+            }
+        }
+        else if (_cableIndex == 2)
+        {
+            if (_LTransforms[_LIndex] == other.transform)
+            {
+                _LIndex += 1;
+                source.PlayOneShot(correctSound);
+
+                if (_LIndex == _LTransforms.Length)
+                {
+                    _cableIndex = 0;
+                }
+            }
+            else 
+            {
+                source.PlayOneShot(errorSound);
+            }
+        }
+        else if (_cableIndex == 3)
+        {
+            if (_SLTransforms[_SLIndex] == other.transform)
+            {
+                _SLIndex += 1;
+                source.PlayOneShot(correctSound);
+
+                if (_SLIndex == _SLTransforms.Length)
+                {
+                    _cableIndex = 0;
+                }
+            }
+            else 
+            {
+                source.PlayOneShot(errorSound);
+            }
+        }
+    }
+}
---
+++ Villa electrica/Assets/Scripts/CircuitRenderer.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircuitRenderer : MonoBehaviour
+{
+    private LineRenderer _lineRenderer;
+    // public CircuitController circuitValues;
+    // GameObject Script = GameObject.Find("Cable");
+    
+    public int SectionID;
+    private int length = 0;
+    private int index = 0;
+
+    [SerializeField] private Transform[] _cableTransforms;
+    public GameObject Cable;
+    // CircuitController CircuitValues = Cable.GetComponent<CircuitController>()._NIndex;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        _lineRenderer = GetComponent<LineRenderer>();
+        
+        
+        // if (SectionID == 1)
+        // {
+            
+            
+        // }
+        // else if (SectionID == 2)
+        // {
+            
+            
+        // }
+        // else if (SectionID == 3)
+        // {
+            
+            
+        // }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (SectionID == 1)
+        {
+            length = Cable.GetComponent<CircuitController>()._NLength;
+            index = Cable.GetComponent<CircuitController>()._NIndex;
+        }
+        else if (SectionID == 2)
+        {
+            length = Cable.GetComponent<CircuitController>()._LLength;
+            index = Cable.GetComponent<CircuitController>()._LIndex;
+        }
+        else if (SectionID == 3)
+        {
+            length = Cable.GetComponent<CircuitController>()._SLLength;
+            index = Cable.GetComponent<CircuitController>()._SLIndex;
+        }
+        _lineRenderer.positionCount = length;
+        for (int i = 0; i < index; i++)
+        {
+            _lineRenderer.SetPosition(i, _cableTransforms[i].position);
+
+        } 
+        // if (Cable.GetComponent<CircuitController>()._cableIndex == SectionID)
+        // {
+        //     _lineRenderer.SetPosition(Cable.GetComponent<CircuitController>()._cableIndex + 1, Cable.transform.position);
+        // }
+        
+    }
+}
